11a171005864384c1bdca9586471e6d0d3e9110b
AneHoi <anehoimark@gmail.com>
2024-02-28 22:26:46 +0100

    buttons added

--- intergration_test/app_test.dart
+++ intergration_test/app_test.dart
@@ -9,6 +9,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:ane_calculator/main.dart';
+import 'package:integration_test/integration_test.dart';
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
---
+++ lib/bll/calc.dart
@@ -0,0 +1,72 @@
+import 'dart:html';
+import 'dart:math';
+
+void main (){
+  Calculator calculator = new Calculator();
+  calculator.push(2);
+  calculator.push(4);
+  calculator.execute(AddCommand());
+  print(calculator.pop());
+}
+
+abstract class Command{
+  apply(List<num> stack);
+}
+
+class Calculator{
+  List<num> stack = [];
+
+  void push(num value) {
+    stack.add(value);
+  }
+
+  void execute(Command command) {
+    command.apply(stack);
+  }
+
+  num pop() {
+    return stack.removeLast();
+  }
+}
+
+class AddCommand implements Command{
+  void apply(List<num> stack){
+    num a = stack.removeLast();
+    num b = stack.removeLast();
+    stack.add(a+b);
+  }
+}
+class SubCommand implements Command{
+  void apply(List<num> stack){
+    num a = stack.removeLast();
+    num b = stack.removeLast();
+    stack.add(a-b);
+  }
+}
+class MultiCommand implements Command{
+  void apply(List<num> stack){
+    num a = stack.removeLast();
+    num b = stack.removeLast();
+    stack.add(a*b);
+  }
+}
+class DivideCommand implements Command{
+  void apply(List<num> stack){
+    num a = stack.removeLast();
+    num b = stack.removeLast();
+    stack.add(a/b);
+  }
+}
+class PowCommand implements Command{
+  void apply(List<num> stack){
+    num a = stack.removeLast();
+    num b = stack.removeLast();
+    stack.add(pow(a,b));
+  }
+}
+class SQRTCommand implements Command{
+  void apply(List<num> stack){
+    num a = stack.removeLast();
+    stack.add(sqrt(a));
+  }
+}
